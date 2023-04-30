04c85a347af63ae350dc676f4525b8b55da56b55
ebayraktaroglu <97638717+ebayraktaroglu@users.noreply.github.com>
2023-04-30 14:45:29 -0700

    fixed a typo

--- expose/javascript/part1-question1.js
+++ expose/javascript/part1-question1.js
@@ -2,7 +2,7 @@ function sumValues(num1, num2, add) {
   if (add) {
     var result = 0;
     result = num1 + num2;
-    console.lod('values added: ', result);
+    console.log('values added: ', result);
   } else return;
   console.log('final result: ', result);
 }
